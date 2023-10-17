df166b7bcbbe93e0fc16b49277dca5999d2ff3f6
Daqsa <36112434+Daqsa@users.noreply.github.com>
2023-10-17 16:35:50 +0900

    added logic to determine who won the game

--- main.js
+++ main.js
@@ -14,7 +14,41 @@ const gameBoard = (function () {
     const setBoard = function (row, col, symbol) {
         board[row][col] = symbol
     };
-    return { getRound, nextRound, getBoard, getSquare, setBoard };
+
+    let gameEnded = false;
+    const getGameEnded = () => gameEnded;
+
+    const checkEndCondition = function () {
+        if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X' || 
+            board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X' || 
+            board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X' || 
+            board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X' || 
+            board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X' || 
+            board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X' || 
+            board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X' || 
+            board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+                gameEnded = true;
+                return 'X';
+        } else if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O' || 
+                   board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O' || 
+                   board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O' || 
+                   board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O' || 
+                   board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O' || 
+                   board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O' || 
+                   board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O' || 
+                   board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
+            gameEnded = true;
+            return 'O';
+        } else if (board[0][0] != '' && board[0][1] != '' && board[0][2] != '' && 
+                   board[1][0] != '' && board[1][1] != '' && board[1][2] != '' && 
+                   board[2][0] != '' && board[2][1] != '' && board[2][2] != '') {
+            gameEnded = true;
+            return 'tie'
+        } else {
+            return 'continue'
+        }
+    }
+    return { getRound, nextRound, getBoard, getSquare, setBoard, getGameEnded, checkEndCondition };
 })();
 
 const displayController = (function () {
@@ -34,11 +68,21 @@ const displayController = (function () {
 const squares = document.querySelectorAll(".square");
 squares.forEach((square) => {
     square.addEventListener("click", (e) => {
+
+        if (gameBoard.getGameEnded()) {
+            alert('The game ended. Please start a new game by refreshing the page.');
+            return;
+        }
+
         let p = e.target.children[0];
         let id = p.id;
         let row = Math.floor(parseInt(id) / 3);
         let col = parseInt(id) % 3;
 
+        if (p.innerText != '') {
+            alert('This square is already set! Please select another square.');
+            return;
+        } 
         // even turns are X, odd are O
         if (gameBoard.getRound() % 2 == 0) {
             gameBoard.setBoard(row, col, 'X');
@@ -46,6 +90,20 @@ squares.forEach((square) => {
         } else {
             gameBoard.setBoard(row, col, 'O');
             p.innerText = 'O';
+        }
+
+        switch(gameBoard.checkEndCondition()) {
+            case 'X':
+                alert('Player X won!');
+                break;
+            case 'O':
+                alert('Player O won!');
+                break;
+            case 'tie':
+                alert('The players tied.');
+                break;
+            default:
+                break;
         }
 
         gameBoard.nextRound();
